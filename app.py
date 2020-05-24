e36f1755c383fe636e0634359f549218bf4bf6a0
Lautaro Castillo Barranco <lautarocastillo.93@gmail.com>
2020-05-24 11:47:11 +0200

    Define function before routing

--- app.py
+++ app.py
@@ -11,20 +11,6 @@ DHT_PIN    = 4
 LDR_SENSOR = 26
 GPIO.setmode(GPIO.BOARD)
 
-@app.route("/")
-def main():
-   humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-   light = rc_time(LDR_SENSOR)
-   templateData = {
-      'temperature': temperature,
-      'humidity':    humidity,
-      'light':       light
-   }
-   return render_template('main.html', **templateData)
-
-if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=8080, debug=True)
-
 def rc_time(ldr_pin):
     count = 0
     GPIO.setup(ldr_pin, GPIO.OUT)
@@ -38,3 +24,18 @@ def rc_time(ldr_pin):
 
     GPIO.cleanup()
     return count
+
+
+@app.route("/")
+def main():
+   humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+   light = rc_time(LDR_SENSOR)
+   templateData = {
+      'temperature': temperature,
+      'humidity':    humidity,
+      'light':       light
+   }
+   return render_template('main.html', **templateData)
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=8080, debug=True)
