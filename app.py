70dd139b273f69ef8660209421fa2743ad6d4ab7
Lautaro Castillo Barranco <lautarocastillo.93@gmail.com>
2020-05-24 12:27:42 +0200

    add json responses

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import Adafruit_DHT
 import RPi.GPIO as GPIO
 import time
@@ -22,11 +22,10 @@ def rc_time(ldr_pin):
     while (GPIO.input(ldr_pin) == GPIO.LOW):
         count += 1
 
-    GPIO.cleanup()
     return count
 
-
 @app.route("/")
+@app.route("/json")
 def main():
    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
    light = rc_time(LDR_SENSOR)
@@ -35,7 +34,25 @@ def main():
       'humidity':    humidity,
       'light':       light
    }
-   return render_template('main.html', **templateData)
+   if request.path == "/json":
+       return templateData
+   else:
+       return render_template('main.html', **templateData)
+
+@app.route("/weather")
+def weather():
+   humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+   return {
+      'temperature': round(temperature, 2),
+      'humidity':    round(humidity, 2)
+   }
+
+@app.route("/light")
+def light():
+   light = rc_time(LDR_SENSOR)
+   return {
+      'light': light
+   }
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8080, debug=True)
